cc285e7aef017ea4642fae98077df2a6b2a574b6
rkomulwad <D4KEXyRPjpo>
2019-04-10 18:28:41 -0400

    integrated helm chart

---
+++ src/main/java/com/sb/sample/sbhelloworld/repo/PersonRepository.java
@@ -0,0 +1,15 @@
+package com.sb.sample.sbhelloworld.repo;
+
+import java.util.List;
+
+import com.sb.sample.sbhelloworld.entity.Person;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "people", path = "people")
+public interface PersonRepository extends PagingAndSortingRepository<Person, Long> {
+
+    List<Person> findByLastName(@Param("name") String name);
+
+}
