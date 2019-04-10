bc932043851afb4246b903b3e317a11fd48bf0d5
rkomulwad <D4KEXyRPjpo>
2019-04-10 11:54:07 -0400

    Sample spring boot code

---
+++ src/main/java/com/sb/sample/sbhelloworld/SbHelloworldApplication.java
@@ -0,0 +1,13 @@
+package com.sb.sample.sbhelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbHelloworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbHelloworldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sb/sample/sbhelloworld/SbHelloworldApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sb.sample.sbhelloworld;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SbHelloworldApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
